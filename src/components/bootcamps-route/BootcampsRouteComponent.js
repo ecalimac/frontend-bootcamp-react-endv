da56b9539821aa9dcfb25618f15dfb8756a27348
ecalimac <elena.calimac@gmail.com>
2020-09-29 17:24:25 +0300

    useReducer, bootcamps state

--- src/components/bootcamps-route/BootcampsRouteComponent.js
+++ src/components/bootcamps-route/BootcampsRouteComponent.js
@@ -1,36 +1,54 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useCallback, useEffect, useReducer, useState } from "react";
 import BootcampForm from "../bootcamp-form/BootcampForm";
 import BootcampsList from "../bootcamps-list/BootcampsList";
 import ErrorModal from "../error-modal/ErrorModal";
 import Search from "../search/Search";
 
+// It is not a good practice to put reducers here, but for a simple demonstration we use them here
+const bootcampReducer = (currentBootcamps, action) => {
+  switch (action.type) {
+    case 'SET':
+      return action.bootcamps;
+    case 'ADD':
+      return [...currentBootcamps, action.bootcamp];
+    case 'DELETE':
+      return currentBootcamps.filter(bootcamp => bootcamp.id !== action.id)
+    default:
+      throw new Error("something went wrong");
+  }
+}
+
+
 const BootcampsRouteComponent = () => {
-  const [bootcamps, setBootcamps] = useState([]);
+  // const [bootcamps, setBootcamps] = useState([]);
+  // useReducer is similar to useState; it receives like parameters the reducer and initial state 
+  const [bootcamps, dispatchBootcamps] = useReducer(bootcampReducer, [])
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
-  useEffect(() => {
-    fetch("https://bootcamp-a8786.firebaseio.com/bootcamps.json")
-      .then((response) => response.json())
-      .then((data) => {
-        const bootcampsFromServer = [];
-        for (const key in data) {
-          bootcampsFromServer.push({
-            id: key,
-            name: data[key].name,
-            description: data[key].description,
-            website: data[key].website,
-            phone: data[key].phone,
-            email: data[key].email,
-          });
-        }
-        console.log(bootcampsFromServer, "bootcamps");
-        // fara al doilea parametru([])urla browserul ca intra intr-un state infinit
-        // la fel urla si daca le punem direct in componenta (fara sa folosim use State)
-        setBootcamps(bootcampsFromServer);
-      });
-  }, []);
-  //Nota: [empty] dat ca dependinta la useEffect face ca useEffect sa se comporte ca si componentDidMount
+  // Note: Actually, we don't need it beacause we make a fetch request in Search even when the searchfield input is empty
+  // useEffect(() => {
+  //   fetch("https://bootcamp-a8786.firebaseio.com/bootcamps.json")
+  //     .then((response) => response.json())
+  //     .then((data) => {
+  //       const bootcampsFromServer = [];
+  //       for (const key in data) {
+  //         bootcampsFromServer.push({
+  //           id: key,
+  //           name: data[key].name,
+  //           description: data[key].description,
+  //           website: data[key].website,
+  //           phone: data[key].phone,
+  //           email: data[key].email,
+  //         });
+  //       }
+  //       console.log(bootcampsFromServer, "bootcamps");
+  //       // fara al doilea parametru([])urla browserul ca intra intr-un state infinit
+  //       // la fel urla si daca le punem direct in componenta (fara sa folosim use State)
+  //       setBootcamps(bootcampsFromServer);
+  //     });
+  // }, []);
+  // //Nota: [empty] dat ca dependinta la useEffect face ca useEffect sa se comporte ca si componentDidMount
   const addBootcamp = (bootcamp) => {
     setIsLoading(true);
     fetch("https://bootcamp-a8786.firebaseio.com/bootcamps.json", {
@@ -46,10 +64,12 @@ const BootcampsRouteComponent = () => {
       })
       .then((data) => {
         // console.log(data);
-        setBootcamps((bootcamps) => [
-          ...bootcamps,
-          { ...bootcamp, id: data.name },
-        ]);
+        // setBootcamps((bootcamps) => [
+        //   ...bootcamps,
+        //   { ...bootcamp, id: data.name },
+        // ]);
+
+        dispatchBootcamps({ type: 'ADD', bootcamp: { id: data.name, ...bootcamp } })
       });
   };
 
@@ -58,7 +78,8 @@ const BootcampsRouteComponent = () => {
   //   setBootcamps(filteredBootcamps);
   // };
   const onSearchHandler = useCallback((filteredBootcamps) => {
-    setBootcamps(filteredBootcamps);
+    // setBootcamps(filteredBootcamps);
+    dispatchBootcamps({ type: 'SET', bootcamps: filteredBootcamps });
   }, []);
 
   const onRemoveBootcamp = (id) => {
@@ -68,9 +89,11 @@ const BootcampsRouteComponent = () => {
     }).then((response) => {
       setIsLoading(false);
       // console.log(response, "delete response");
-      setBootcamps((prevBootcamp) =>
-        prevBootcamp.filter((bootcamp) => bootcamp.id !== id)
-      );
+      // setBootcamps((prevBootcamp) =>
+      //   prevBootcamp.filter((bootcamp) => bootcamp.id !== id)
+      // );
+      dispatchBootcamps({ type: 'DELETE', id });
+
     }).catch((error) => {
       setIsLoading(false);
       console.log(error, "eroare");
